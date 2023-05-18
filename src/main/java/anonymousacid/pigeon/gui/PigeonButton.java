1c3c361c04a89d4d149b4a873e0b27f7407dc38e
AnonymousAcid <anonymousacid14@gmail.com>
2023-05-18 16:38:50 -0700

    ACTUALLY got dragging and clicking working

--- src/main/java/anonymousacid/pigeon/gui/PigeonButton.java
+++ src/main/java/anonymousacid/pigeon/gui/PigeonButton.java
@@ -14,8 +14,9 @@ public class PigeonButton extends GuiButton {
 	public boolean pressed = false;
 	public boolean dragged = false;
 	
-	private int pressedMouseX = 0;
-	private int pressedMouseY = 0;
+	//These are positions based on the middle of the button.
+	private int pressedX = 0;
+	private int pressedY = 0;
 
 	public PigeonButton(int buttonId, int x, int y, int buttonSize, String buttonText) {
 		super(buttonId, x, y, buttonSize, buttonSize, buttonText);
@@ -24,44 +25,65 @@ public class PigeonButton extends GuiButton {
 	@Override
 	protected void mouseDragged(Minecraft mc, int mouseX, int mouseY) {
 		if(pressed && Mouse.isButtonDown(0)) {
-			double distFromPressedPos = Math.sqrt(
-					Math.pow(mouseX - pressedMouseX, 2) + Math.pow(mouseY - pressedMouseY, 2) 
-					);
 			
-			if(distFromPressedPos > width) {
+			//If not dragged yet, constantly check if it is being dragged (true when going out of button bounds)
+			if(!dragged) {
+				double distFromPressedPos = Math.sqrt(
+						Math.pow(mouseX - pressedX, 2) + Math.pow(mouseY - pressedY, 2) 
+						);
+				
+				dragged = distFromPressedPos > width/2;
+			}
+			else {//If being dragged, then move to mouse.
 				xPosition = mouseX-width/2;
 				yPosition = mouseY-height/2;
-				dragged = true;
 			}
+			
 		}
 	}
 	
 	@Override
 	public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
-		pressed = this.enabled && this.visible && mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+		pressed = enabled && 
+				visible && mouseX >= xPosition && mouseY >= yPosition &&
+				mouseX < xPosition + width && mouseY < yPosition + height;
 		
 		Utils.sendMessage("pressed");
 		
-		pressedMouseX = mouseX;
-		pressedMouseY = mouseY;
+		pressedX = xPosition + width/2;
+		pressedY = yPosition + height/2;
 		
 		//Return false to prevent opening advancements screen when clicked in player inventory
 		return false;
 	}
 	
 	@Override
-	public void mouseReleased(int mouseX, int mouseY) {
-		Utils.sendMessage("mouse released");
-		if(pressed && !dragged) {
-			Minecraft.getMinecraft().displayGuiScreen(new ConfigGui());
-		}
-		else if (pressed && dragged) {
-			ConfigHandler.writeInt(ConfigHandler.MISCELLANEOUS_CATEGORY, "pigeonButtonX", xPosition);
-			ConfigHandler.writeInt(ConfigHandler.MISCELLANEOUS_CATEGORY, "pigeonButtonY", yPosition);
-		}
+	public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+		super.drawButton(mc, mouseX, mouseY);
 		
-		dragged = false;
-		pressed = false;
+		//if mouse released
+		if(pressed && !Mouse.isButtonDown(0)) {
+			Utils.sendMessage("mouse released");
+			
+			if(!dragged) {//if was pressed, but not dragged
+				Minecraft.getMinecraft().displayGuiScreen(new ConfigGui()); //Show pigeon config Gui
+			}
+			else if (dragged) {//if pressed, and dragged 
+				//save the position the button was dragged to
+				ConfigHandler.writeInt(ConfigHandler.MISCELLANEOUS_CATEGORY, "pigeonButtonX", xPosition);
+				ConfigHandler.writeInt(ConfigHandler.MISCELLANEOUS_CATEGORY, "pigeonButtonY", yPosition);
+			}
+			
+			dragged = false;
+			pressed = false;
+		}
 	}
 	
+	
+	//THIS METHOD DOESNT GET CALLED AT ALL. The comments in MC source code lied >:(
+	@Override
+	public void mouseReleased(int mouseX, int mouseY) {
+		super.mouseReleased(mouseX, mouseY);
+		
+	}
 }
