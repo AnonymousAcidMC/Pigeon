0356d8f949189f5fe750e31096b15e3a81db8a9b
AnonymousAcid <anonymousacid14@gmail.com>
2023-06-03 16:06:39 -0700

    Add 1.19.4 implementation of McIf class

--- other versions/Pigeon MC-1.19.4/src/main/java/io/github/anonymousacid/pigeon/McIf.java
+++ other versions/Pigeon MC-1.19.4/src/main/java/io/github/anonymousacid/pigeon/McIf.java
@@ -1,5 +1,41 @@
 package io.github.anonymousacid.pigeon;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.level.Level;
+
 public class McIf {
-    
+    public static Minecraft mc;
+
+    /**
+     * Called when this mod is initialized
+     * Having this prevents the constant method calls of "Minecraft.getMineraft()"
+     */
+    public static void setMinecraftSingleton() {
+        mc = Minecraft.getInstance();
+    }
+
+    /**
+     * Returns the whole Minecraft Instance
+     * @return
+     */
+    public static Minecraft minecraft() {
+        return Minecraft.getInstance();
+    }
+
+    /**
+     * Returns the world object on the client.
+     * @return
+     */
+    public static Level world() {
+        return mc.player.level;
+    }
+
+    /**
+     * Returns the user player object
+     * @return
+     */
+    public static LocalPlayer player() {
+        return mc.player;
+    }
 }
