75235766cc09f4d43e83185429e069e1c79511ed
AnonymousAcid <anonymousacid14@gmail.com>
2023-06-03 15:58:15 -0700

    Set up CommandInit to register new BaseCommands

---
+++ other versions/Pigeon MC-1.19.4/src/main/java/io/github/anonymousacid/pigeon/command/impl/TestCommand.java
@@ -0,0 +1,5 @@
+package io.github.anonymousacid.pigeon.command.impl;
+
+public class TestCommand {
+    
+}
--- other versions/Pigeon MC-1.19.4/src/main/java/io/github/anonymousacid/pigeon/init/CommandInit.java
+++ other versions/Pigeon MC-1.19.4/src/main/java/io/github/anonymousacid/pigeon/init/CommandInit.java
@@ -1,11 +1,21 @@
 package io.github.anonymousacid.pigeon.init;
 
+import com.mojang.brigadier.CommandDispatcher;
 import io.github.anonymousacid.pigeon.command.BaseCommand;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraftforge.event.RegisterCommandsEvent;
 
 import java.util.ArrayList;
 
 public class CommandInit {
     private static final ArrayList<BaseCommand> commands = new ArrayList<BaseCommand>();
 
-
+    public static void registerCommands(final RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+        commands.forEach(command -> {
+            if(command.isEnabled() && command.setExecution() != null) {
+                dispatcher.register(command.getLiteralArgumentBuilder());
+            }
+        });
+    }
 }
