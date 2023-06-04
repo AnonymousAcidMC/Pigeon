d2ccf90c8ede7dc7d1dc920724f1957dd02d49e0
AnonymousAcid <anonymousacid14@gmail.com>
2023-06-03 20:08:31 -0700

    Remove comments

--- other versions/Pigeon-MC1.19.4/src/main/java/io/github/anonymousacid/pigeon/Pigeon.java
+++ other versions/Pigeon-MC1.19.4/src/main/java/io/github/anonymousacid/pigeon/Pigeon.java
@@ -13,12 +13,11 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Pigeon.MODID)
 public class Pigeon {
-    // Define mod id in a common place for everything to reference
+
     public static final String MODID = "pigeon";
-    // Directly reference a slf4j logger
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Pigeon() {
