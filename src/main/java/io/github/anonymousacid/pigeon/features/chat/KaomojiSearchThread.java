4b39c26b7e3857f76faa58f02d56b1e4a3ffc55c
AnonymousAcid <anonymousacid14@gmail.com>
2023-06-16 18:21:43 -0700

    Create KaomojiSearchThread

---
+++ src/main/java/io/github/anonymousacid/pigeon/features/chat/KaomojiSearchThread.java
@@ -0,0 +1,63 @@
+package io.github.anonymousacid.pigeon.features.chat;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import io.github.anonymousacid.pigeon.utils.Utils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class KaomojiSearchThread extends Thread {
+    private ArrayList<String> matches = new ArrayList<String>();
+    private Gson gson = new Gson();
+    private JsonArray kaomojiJsonArray = null;
+    private String searchQuery;
+
+    private boolean doneSearching;
+
+    public KaomojiSearchThread(String searchQuery) {
+        this.searchQuery = searchQuery.toLowerCase();
+    }
+
+    @Override
+    public void run() {
+        InputStream in = this.getClass().getResourceAsStream("/assets/pigeon/data/kaomojis.json");
+        try {
+            //load kaomojis if haven't done so. this prevents the thread from constantly loading the file's contents
+            if(kaomojiJsonArray == null) {
+                byte[] fileBytes = IOUtils.toByteArray(in);
+                String fileContents = new String(fileBytes, StandardCharsets.UTF_8);
+                JsonArray kaomojiJsonArray = gson.fromJson(fileContents, JsonArray.class);
+            }
+
+            //loop through json array of kaomojis to search for matches
+            for(JsonElement element : kaomojiJsonArray) {
+                JsonObject obj = element.getAsJsonObject();
+                JsonArray tags = obj.get("tags").getAsJsonArray(); //the search tags for this kaomoji
+
+                //loop through tags to see if it matches search query
+                for(int i = 0; i < tags.size(); i++) {
+                    //if a search tag matches the search query, add this kaomoji match
+                    if(tags.get(i).getAsString().toLowerCase().startsWith(searchQuery)) {
+                        matches.add(obj.get("string").getAsString());
+                        break;//break out of loop when added this search match
+                    }
+                }
+
+            }
+        } catch(Exception err) {
+            Utils.sendMessage(err.getMessage()); err.printStackTrace();
+        }
+
+        doneSearching = true;
+    }
+
+    public ArrayList<String> getMatches() {
+        return doneSearching ? matches : null;
+    }
+
+}
--- src/main/java/io/github/anonymousacid/pigeon/features/chat/Kaomojis.java
+++ src/main/java/io/github/anonymousacid/pigeon/features/chat/Kaomojis.java
@@ -6,6 +6,7 @@ import io.github.anonymousacid.pigeon.handlers.ConfigHandler;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import java.util.ArrayList;
 
 import static io.github.anonymousacid.pigeon.McIf.mc;
 
@@ -16,19 +17,25 @@ public class Kaomojis {
 
     public static boolean showSuggestions = true;
 
+    public static KaomojiSearchThread searchThread = null;
+
+    public static ArrayList<String> kaomojiMatches = new ArrayList<String>();
+
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event) {
         if(!ConfigHandler.chatKaomojis) return;
+        if(event.type != TickEvent.Type.CLIENT) return;
 
-
+        
     }
 
     @SubscribeEvent
     public void onRenderGui(RenderGameOverlayEvent.Post event) {
+        if(!showSuggestions) return;
         if(!ConfigHandler.chatKaomojis) return;
-        if(event.type == RenderGameOverlayEvent.ElementType.CHAT) {
+
+        if(event.type == RenderGameOverlayEvent.ElementType.CHAT)
             KaomojiSuggestionGui.render();
-        }
     }
 
 }
